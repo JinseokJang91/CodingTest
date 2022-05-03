a591c8d3d43553e31cc7e0399d0e9887f15d52f8
JinSeokJang91 <hya2089@gmail.com>
2022-05-03 19:32:22 +0900

    20220503CT
    월간 코드 챌린지 시즌2>음양 더하기

---
+++ src/programmers/allProblems/LV1_11.java
@@ -0,0 +1,35 @@
+package programmers.allProblems;
+
+public class LV1_11 {
+
+	public static void main(String[] args) {
+		SolutionLV1_11 sol = new SolutionLV1_11();
+		//int[] absolutes = {4,7,12};
+		//boolean[] signs = {true,false,true};
+		int[] absolutes = {1,2,3};
+		boolean[] signs = {false,false,true};
+		int answer = sol.solution(absolutes, signs);
+
+	}
+
+}
+
+class SolutionLV1_11 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 123456789;
+        
+        //월간 코드 챌린지 시즌2 > 음양 더하기
+        int sum = 0;
+        for(int i=0; i<absolutes.length; i++) {
+        	if(signs[i] == true) {
+        		sum += absolutes[i];
+        	}else {
+        		sum -= absolutes[i];
+        	}
+        }
+        System.out.println("sum : " + sum);
+        answer = sum;
+        
+        return answer;
+    }
+}

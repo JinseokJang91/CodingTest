7fe9880340722cd7e170377bd9dd43593ae67a52
장진석 <85108311+JinseokJang91@users.noreply.github.com>
2022-05-26 19:56:05 +0900

    20220526CT

--- src/programmers/allProblems/lv1/LV1_28.java
+++ src/programmers/allProblems/lv1/LV1_28.java
@@ -3,7 +3,19 @@ package programmers.allProblems.lv1;
 public class LV1_28 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//¿¬½À¹®Á¦ > Â¦¼ö¿Í È¦¼ö
+		String answer = "";
+        int num = 3;
+        
+        //0Àº Â¦¼ö·Î Ãë±Þ
+        if(num % 2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        
+        System.out.println("answer : " + answer);
 
 	}
 

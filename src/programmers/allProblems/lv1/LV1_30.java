8b0b6e17e03eb5eda2c390c505b3608bd5595536
JinSeokJang91 <hya2089@gmail.com>
2022-05-28 23:41:16 +0900

    20220528CT
    연습문제 > 제일 작은 수 제거하기

---
+++ src/programmers/allProblems/lv1/LV1_30.java
@@ -0,0 +1,18 @@
+package programmers.allProblems.lv1;
+
+public class LV1_30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class SolutionLV1_30 {
+    public int[] solution(int[] arr) {
+    	//연습문제 > 제일 작은 수 제거하기
+        int[] answer = {};
+        return answer;
+    }
+}

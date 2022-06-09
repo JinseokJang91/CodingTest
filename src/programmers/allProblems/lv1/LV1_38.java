8d48e4887ba04c6eef7fe8364409a724b185adce
장진석 <85108311+JinseokJang91@users.noreply.github.com>
2022-06-09 19:45:19 +0900

    20220609CT

--- src/programmers/allProblems/lv1/LV1_38.java
+++ src/programmers/allProblems/lv1/LV1_38.java
@@ -3,8 +3,25 @@ package programmers.allProblems.lv1;
 public class LV1_38 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SolutionLV1_38 sol = new SolutionLV1_38();
+		int n = 987;
+		int answer = sol.solution(n);
+		System.out.println("answer : " + answer);
 
 	}
 
+}
+
+class SolutionLV1_38 {
+    public int solution(int n) {
+    	//연습문제 > 자릿수 더히기
+    	int answer = 0;
+
+    	String val = String.valueOf(n);
+    	for(int i=0; i<val.length(); i++) {
+    		answer += Integer.valueOf(String.valueOf(val.charAt(i)));
+    	}
+
+        return answer;
+    }
 }

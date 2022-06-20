f87cd5412bc8adc4f36097ba733d8b5994305735
장진석 <85108311+JinseokJang91@users.noreply.github.com>
2022-06-20 21:28:24 +0900

    20220620CT
    백준 2739번

---
+++ src/baekjoon/allProblems_baekjoon/No_2739.java
@@ -0,0 +1,19 @@
+package baekjoon.allProblems_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No_2739 {
+
+	public static void main(String[] args) throws Exception{
+		
+		//2739번 구구단
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(N + " * " + i + " = " + (N * i));
+		}
+	}
+}

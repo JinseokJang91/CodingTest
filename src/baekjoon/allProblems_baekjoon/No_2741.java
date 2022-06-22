e7c41d37daaef9e9d23c10d56e7ab3b516168249
장진석 <85108311+JinseokJang91@users.noreply.github.com>
2022-06-22 23:08:34 +0900

    20220622CT - 백준 2741번

---
+++ src/baekjoon/allProblems_baekjoon/No_2741.java
@@ -0,0 +1,18 @@
+package baekjoon.allProblems_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No_2741 {
+
+	public static void main(String[] args) throws Exception{
+		
+		//2741¹ø NÂï±â
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=N; i++) {
+			System.out.println(i);
+		}
+	}
+}

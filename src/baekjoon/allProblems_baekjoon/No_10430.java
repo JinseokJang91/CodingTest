d067a4161846b1a8493a38d058ac0f0e0123a964
JinSeokJang91 <hya2089@gmail.com>
2022-06-24 22:53:18 +0900

    20220624CT
    백준 10430

---
+++ src/baekjoon/allProblems_baekjoon/No_10430.java
@@ -0,0 +1,25 @@
+package baekjoon.allProblems_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No_10430 {
+
+	public static void main(String[] args) throws Exception{
+		
+		//10430 ³ª¸ÓÁö
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		
+		String[] arr = input.split(" ");
+		int A = Integer.parseInt(arr[0]);
+		int B = Integer.parseInt(arr[1]);
+		int C = Integer.parseInt(arr[2]);
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+	}
+}

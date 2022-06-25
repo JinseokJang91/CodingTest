2f2f2bf5a2f1e12c43810d33a4576ee49c1638f4
JinSeokJang91 <hya2089@gmail.com>
2022-06-25 15:05:29 +0900

    20220625CT
    백준 11654번

---
+++ src/baekjoon/allProblems_baekjoon/No_11654.java
@@ -0,0 +1,23 @@
+package baekjoon.allProblems_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No_11654 {
+
+	public static void main(String[] args) throws Exception{
+		
+		//11654 아스키 코드
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		char result = input.charAt(0);
+		
+		if(result >= 0 && result <= 9) {
+			System.out.println((char)result);
+		}else {
+			System.out.println((int)result);
+		}
+		
+	}
+}
